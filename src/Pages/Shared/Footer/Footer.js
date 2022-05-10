6377516b12ea32f5b23fea0a96117df526394c42
parvez13913 <parvezz13913@gmail.com>
2022-05-11 02:58:05 +0600

    Footer Complite

---
+++ src/Pages/Shared/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div>
+            <div className='bg-dark p-5 fw-bold'>
+                <h1 className='text-secondary text-center'> Basic PhotoGraphy</h1>
+                <div className=' container w-100 text-center'>
+                    <p className='text-secondary'>Basic PhotoGraphy is Service website.Photography is the art, application, and practice of creating durable images by recording light, either electronically by means of an image sensor, or chemically by means of a light-sensitive material such as photographic film.</p>
+                    <h5 className='text-secondary text-center fw-bold'>Contact Us</h5>
+                    <h5 className='text-secondary'>Email: parvezz13913@gmail.com</h5>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
