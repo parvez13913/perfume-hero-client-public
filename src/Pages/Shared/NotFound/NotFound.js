33d402dddc48d1cb22711f735286ba13563bac5c
parvez13913 <parvezz13913@gmail.com>
2022-05-12 03:35:59 +0600

    NotFound Page Complite

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './NotFound.css';
+import notFoundImage from '../../../images/not-found-pag.png';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>This is Not Found page</h1>
+        <div className='not-found'>
+            <div className='container d-flex align-items-center justify-content-center'>
+                <div className='notfound-img d-block ml-5'>
+                    <img src={notFoundImage} alt="" />
+                </div>
+                <div className='mx-auto text-center not-found-title'>
+                    <h1 className='text-white font-size text-center'>404</h1>
+                    <h5 className='text-center'>Sorry, we can't find that page! Don't worry though, everything is STILL AWESOME!</h5>
+                    <Link to='/home'>
+                        <button className='back-btn mt-2'>Go Back</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 };
