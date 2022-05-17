227d17b54956d51efe2f860e7c726ec4c380e0e9
parvez13913 <parvezz13913@gmail.com>
2022-05-17 16:48:30 +0600

    NavBar fix

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -14,38 +14,36 @@ const Header = () => {
         signOut(auth);
     }
     return (
-        <div>
-            <Navbar className='sticky-top' sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand as={Link} to="/">
-                        <div className='d-flex align-items-center justify-content-center'>
-                            <img className='header-logo' src={logo} alt="" />
-                            <h6 className='text-secondary mt-2'>Perfume Hero</h6>
-                        </div>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link as={Link} to="/">Home</Nav.Link>
-                            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
-                            <Nav.Link as={Link} to="/about">About</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            {
-                                user ? <p className='pointer text-secondary m-2' onClick={handelLogout}>Logout</p> : <Nav.Link as={Link} to="Login">Login</Nav.Link>
-                            }
-                            <Nav.Link as={Link} to="additems">AddItems</Nav.Link>
-                            {
-                                user && <>
-                                    <Nav.Link as={Link} to="manage">ManageInventory</Nav.Link>
-                                    <Nav.Link as={Link} to="myItem">MyItem</Nav.Link>
-                                </>
-                            }
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </div>
+        <Navbar className='sticky-top' sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    <div className='d-flex align-items-center justify-content-center'>
+                        <img className='header-logo' src={logo} alt="" />
+                        <h6 className='text-secondary mt-2'>Perfume Hero</h6>
+                    </div>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link as={Link} to="/">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                    </Nav>
+                    <Nav>
+                        {
+                            user ? <p className='pointer text-secondary m-2' onClick={handelLogout}>Logout</p> : <Nav.Link as={Link} to="Login">Login</Nav.Link>
+                        }
+                        <Nav.Link as={Link} to="additems">AddItems</Nav.Link>
+                        {
+                            user && <>
+                                <Nav.Link as={Link} to="manage">ManageInventory</Nav.Link>
+                                <Nav.Link as={Link} to="myItem">MyItem</Nav.Link>
+                            </>
+                        }
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
 };
 
